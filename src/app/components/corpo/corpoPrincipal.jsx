13a88d4d1f124565848fc785d1f7100f502f0dbe
Felipe_Souza <felipevieira67.fs@gmail.com>
2025-01-27 19:28:35 -0300

    corrige addEventListener por oSubmit

--- src/app/components/corpo/corpoPrincipal.jsx
+++ src/app/components/corpo/corpoPrincipal.jsx
@@ -7,7 +7,7 @@ import { DivPrincipal, EntraDados, Obrigatorio, Input, BotaoCalcular } from "./c
 export function Corpo(){
 
     const [salario, setSalario] = useState(0);
-    const [ganhoAnual, setGanhoAnual] = useState()
+    const [ganhoAnual, setGanhoAnual] = useState(null)
 
     const calculaSalario = () =>{
         const salarioAnual = salario * 13; //meses do ano mais decimo terceiro salario
@@ -19,13 +19,13 @@ export function Corpo(){
     
     const dinheiroFormatado =(x)=> new Intl.NumberFormat('pt-BR',{style:'currency',currency:'BRL'}).format(x)
     
-    addEventListener('submit', (e)=>{
-        e.preventDefault()
-        calculaSalario()
-    })
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        calculaSalario();
+    }
     return(
     <DivPrincipal>
-        <EntraDados>
+        <EntraDados onSubmit={handleSubmit}>
             <h2>Digite seu salário <Obrigatorio>*</Obrigatorio></h2>
             <Input type="number" onChange={e=>setSalario(Number(e.target.value))}/>
             <br/>
