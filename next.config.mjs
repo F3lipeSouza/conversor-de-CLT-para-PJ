46f0d779c8ba57498c5a6434dccc192c0876d27f
Felipe_Souza <felipevieira67.fs@gmail.com>
2025-01-27 20:04:59 -0300

    corrige next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output:"export",
+    basePath:"/conversor-de-CLT-para-PJ",
+    trailingSlash: true,
+};
 
 export default nextConfig;
