0920e2f69aca7a467d73b56d32eb8172f2135930
Felipe_Souza <felipevieira67.fs@gmail.com>
2025-01-31 22:54:14 -0300

    altera json e config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/conversor-de-CLT-para-PJ",
-    assetPrefix: "/conversor-de-CLT-para-PJ",
-    output:"export",
-    reactStrictMode: true
-};
-
-export default nextConfig;
+    output: 'export',
+    images: {
+      unoptimized: true 
+    },
+    basePath: process.env.NODE_ENV === 'production' ? 'https://github.com/F3lipeSouza/conversor-de-CLT-para-PJ ':'', 
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://github.com/F3lipeSouza/conversor-de-CLT-para-PJ' : ''
+  }
+  
+  module.exports = nextConfig
