53dc90a38737ece845e97c234c75ebf5867baeee
Felipe_Souza <felipevieira67.fs@gmail.com>
2025-01-31 22:41:44 -0300

    adiciona assetprefix no next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/conversor-de-CLT-para-PJ",
+    assetPrefix: "/conversor-de-CLT-para-PJ",
     output:"export",
     reactStrictMode: true
 };
