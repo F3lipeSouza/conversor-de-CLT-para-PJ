fb5986f98efe9d89167f20421a9a6438b197186f
Felipe_Souza <felipevieira67.fs@gmail.com>
2025-01-31 23:10:45 -0300

    trata deploy no gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@
 const nextConfig = {
     output: 'export',
     images: {
-      unoptimized: true 
+        unoptimized: true
     },
-    basePath: process.env.NODE_ENV === 'production' ? 'https://github.com/F3lipeSouza/conversor-de-CLT-para-PJ ':'', 
-    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://github.com/F3lipeSouza/conversor-de-CLT-para-PJ' : ''
-  }
-  
-  module.exports = nextConfig
+    basePath: process.env.NODE_ENV === 'production' ? '/conversor-de-CLT-para-PJ' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/conversor-de-CLT-para-PJ' : ''
+}
+
+export default nextConfig;
